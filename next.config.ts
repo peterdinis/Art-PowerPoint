0dc56fb1445836c12eead01e34f3f4784d79d5a2
peterdinis <petter1@pobox.sk>
2026-02-06 19:12:58 +0100

    infra: Update next-config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,48 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	reactStrictMode: true,
+	reactCompiler: true,
+	devIndicators: {
+		position: "bottom-right",
+	},
+
+	typedRoutes: true,
+	experimental: {
+		optimizeCss: true,
+		optimizePackageImports: ["lucide-react", "framer-motion", "zustand"],
+		browserDebugInfoInTerminal: true,
+	},
+	images: {
+		formats: ['image/webp'],
+		deviceSizes: [640, 750, 828, 1080, 1200],
+		imageSizes: [16, 32, 64, 96, 128, 256],
+		minimumCacheTTL: 300,
+	},
+	poweredByHeader: false,
+	generateEtags: true,
+	compress: true,
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'X-DNS-Prefetch-Control',
+						value: 'on',
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN',
+					},
+				],
+			},
+		];
+	}
 };
 
 export default nextConfig;
